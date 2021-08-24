7af0328213f3e98b3ec94de3c2b7e0748d81ecaa
Robert-Phan <fannk987@gmail.com>
2021-08-24 18:26:39 +0700

    turning done, update() FIXED!

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main_loop(stdscr: curses.window):
     curses.curs_set(0)
         
     i = 0
-    g = Tetris([(0,0), (0,1), (1,0), (1,1)], 
+    g = Tetris([(0,0), (0,1), (0, 2), (1, 2)], 
                1,
                stdscr)
     # * loop 
@@ -17,15 +17,16 @@ def main_loop(stdscr: curses.window):
         stdscr.refresh()
         c = stdscr.getch()
         if c == 3: break # Ctrl + c == break
-        
-        if c == 97: g.update(g.move_left)
+        # * controls
+        if c == 97:  g.update(g.move_left)
         if c == 100: g.update(g.move_right)
-        
-        if c == 115: g.update(g.turn_right)
+        if c == 115: g.update(g.fall)
+        if c == 261: g.update(lambda: g.turn(1.570796))  #left turn
+        if c == 260: g.update(lambda: g.turn(-1.570796)) #right turn
         
         # * natural block falling
         i += 1
-        if i == 50:
+        if i == 55:
             g.update(g.fall)
             i = 0
         time.sleep(0.01)
--- tetris_class.py
+++ tetris_class.py
@@ -27,43 +27,43 @@ class Tetris:
     def move_right(self):
         return [(x+1,y) for (x,y) in self.coords]
 
-    def turn_right(self):
-        cos_theta = cos(1.570796)
-        sin_theta = sin(1.570796)
+    def turn(self, radians):
+        cos_theta = cos(radians) #for calculations
+        sin_theta = sin(radians)
         
-        pivot = self.coords[self.pivot_point]
-        (pivot_x, pivot_y) = pivot
+        (pivot_x, pivot_y) = self.coords[self.pivot_point]
         
         new_coords = []
         for (x,y) in self.coords:
+            # I found these calculations online
             new_x = cos_theta * (x - pivot_x) - sin_theta * (y - pivot_y) + pivot_x
             new_y = sin_theta * (x - pivot_x) + cos_theta * (y - pivot_y) + pivot_y
             new_coords.append((round(new_x), round(new_y)))
             
         return new_coords
     
+    
     def print(self):
         ...
     def update(self, update_func: Callable[[], list]):
         # * deletes previous block
-        for i, (x, ys) in enumerate(Tetris.convert(self.coords)):
+        for x, ys in sorted(Tetris.convert(self.coords)):
+            first_x = sorted(Tetris.convert(self.coords))[0][0]
             for y in ys:
-                self.screen.addstr(y, x + i, " ")
-                self.screen.addstr(y, x + i + 1, " ")
+                self.screen.addstr(y, first_x + (x-first_x)*2, "  ")
 
         curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_RED)
         new_coords = update_func() 
-        for i, (x, ys) in enumerate(Tetris.convert(new_coords)):
+        # * add in new coords
+        for x, ys in sorted(Tetris.convert(new_coords)):
+            first_x = sorted(Tetris.convert(new_coords))[0][0]
             for y in ys:
-                self.screen.addstr(y, x + i, " ", curses.color_pair(1))
-                self.screen.addstr(y, x + i + 1, " ", curses.color_pair(1))
+                self.screen.addstr(y, first_x + (x-first_x)*2, "  ", curses.color_pair(1))
             
         self.coords = new_coords
 
-g = Tetris([(0,0), (0,1), (0, 2), (1, 2)], 
+g = Tetris([(10,10), (10,11), (10, 12), (11, 12)], 
                1,
                curses.initscr())
 
-print(g.turn_right())
-print(g.turn_right())
-print(g.turn_right())
+
